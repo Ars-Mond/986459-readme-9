09f0b7084a403253fd917301c5070136b025852e
Arsenii <ars123.www@gmail.com>
2019-04-23 21:52:51 +0500

    Шаблонизация проекта
    Поправил массивы

--- index.php
+++ index.php
@@ -4,4 +4,4 @@ require_once('functions.php');
 require_once('helpers.php');
 
 $content = include_template('index.php', ['posts' => $posts]);
-echo include_template('layout.php', ['page_content' => $content]);
+echo include_template('layout.php', ['page_content' => $content, 'is_auth' => $is_auth, 'user_name' => $user_name]);
